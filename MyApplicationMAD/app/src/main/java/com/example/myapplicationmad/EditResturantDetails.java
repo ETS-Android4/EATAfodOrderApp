301f090386b7110a31f00bd341a6f19f8530bdc1
Umesh Madushan <umeshmadushan665@gmail.com>
2022-05-06 14:52:19 +0530

    food app
    food app

---
+++ MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/EditResturantDetails.java
@@ -0,0 +1,14 @@
+package com.example.myapplicationmad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditResturantDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_resturant_details);
+    }
+}
---
+++ MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/ProductAdd.java
@@ -0,0 +1,14 @@
+package com.example.myapplicationmad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_add);
+    }
+}
