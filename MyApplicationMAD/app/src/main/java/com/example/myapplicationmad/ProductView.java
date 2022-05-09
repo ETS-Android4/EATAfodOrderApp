745cdcc8a0afdb698fe70f85110ad57fab8679b2
Umesh Madushan <umeshmadushan665@gmail.com>
2022-05-09 22:02:12 +0530

    navigate_2
    navigate_2

--- MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/ProductView.java
+++ MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/ProductView.java
@@ -2,13 +2,27 @@ package com.example.myapplicationmad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProductView extends AppCompatActivity {
+    private Button Addproduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_view);
+        Addproduct= (Button)findViewById(R.id.addproduct);
+
+        Addproduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ProductView.this,ProductAdd.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
