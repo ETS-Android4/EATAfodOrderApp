798f7de22b5df0244fcc8b4e7354b6af6bce7f75
Umesh Madushan <umeshmadushan665@gmail.com>
2022-05-11 15:50:38 +0530

    navigate_my_part
    navigate_my_part

--- MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/ProductView.java
+++ MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/ProductView.java
@@ -8,18 +8,28 @@ import android.view.View;
 import android.widget.Button;
 
 public class ProductView extends AppCompatActivity {
-    private Button Addproduct;
+    private Button Addproduct,EditResturantDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_view);
         Addproduct= (Button)findViewById(R.id.addproduct);
+        EditResturantDetails=(Button)findViewById(R.id.editProductDetails);
 
         Addproduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(ProductView.this,ProductAdd.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        EditResturantDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ProductView.this,EditResturantDetails.class);
                 startActivity(intent);
                 finish();
             }
