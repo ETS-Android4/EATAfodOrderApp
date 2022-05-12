8e49422c83ecc0161823e9e5d6e4c3e90450623f
Umesh Madushan <umeshmadushan665@gmail.com>
2022-05-12 18:02:24 +0530

    my part
    my part

--- MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/AddShopDetails.java
+++ MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/AddShopDetails.java
@@ -2,13 +2,30 @@ package com.example.myapplicationmad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddShopDetails extends AppCompatActivity {
+
+    private Button SubmitAddResturant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_shop_details);
+
+        SubmitAddResturant= (Button)findViewById(R.id.submit_addResturant);
+
+        SubmitAddResturant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(AddShopDetails.this,SellarAccount.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/EditResturantDetails.java
+++ MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/EditResturantDetails.java
@@ -8,22 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class EditResturantDetails extends AppCompatActivity {
-    private Button saveResturantDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_resturant_details);
 
-        saveResturantDetails=(Button)findViewById(R.id.save_addProduct);
 
-        saveResturantDetails.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(EditResturantDetails.this,ProductView.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 }
