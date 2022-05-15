d8201974732249a57ee4f8304ef532f4e1e890bf
SManeesha99 <it20649012@my.sliit.lk>
2022-05-16 01:13:40 +0530

    Revert "Merge branch 'manee' of https://github.com/umeshmadushan/EATAfodOrderApp into manee"
    This reverts commit bf8b726f58041f3b604846db3da2c22e202a05ec, reversing
    changes made to 48cf76ac1ebff9d225ecd5dc52893fd44225c647.

--- MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/ProductAdd.java
+++ MyApplicationMAD/app/src/main/java/com/example/myapplicationmad/ProductAdd.java
@@ -14,7 +14,7 @@ public class ProductAdd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_add);
-        SubmitAddproduct= (Button)findViewById(R.id.submit_addResturant);
+        SubmitAddproduct= (Button)findViewById(R.id.save_addProduct);
 
         SubmitAddproduct.setOnClickListener(new View.OnClickListener() {
             @Override
